b78b12523bab78be595cda5273029e5a5ee118ad
Yuning <davidliudev@gmail.com>
2022-07-05 15:15:26 +0800

    Add parameter to deployment

--- scripts/deployEth.js
+++ scripts/deployEth.js
@@ -1,14 +1,17 @@
 const { ethers } = require("hardhat");
 
 const MintableERC20PredicateProxyAddTestNet = "0x37c3bfC05d5ebF9EBb3FF80ce0bd0133Bf221BC8";
+const MintableERC20PredicateProxyAddMainNet = "0x9923263fA127b3d1484cFD649df8f1831c2A74e4";
+const ADMIN_WALLET_ADDRESS = process.env.ADMIN_WALLET_ADDRESS
 
 async function main() {
+    console.log("ADMIN WALLET:", ADMIN_WALLET_ADDRESS)
     const [deployer] = await ethers.getSigners();
     console.log("Deploying contracts with the account:", deployer.address);
     console.log("Account balance:", (await deployer.getBalance()).toString());
 
     const TypeVEnergyEthContract = await ethers.getContractFactory("TypeVEnergyEth");
-    const token = await TypeVEnergyEthContract.deploy([], MintableERC20PredicateProxyAddTestNet)
+    const token = await TypeVEnergyEthContract.deploy([], ADMIN_WALLET_ADDRESS, MintableERC20PredicateProxyAddMainNet)
 
     console.log("Token address:", token.address)
 }
--- scripts/deployPolygon.js
+++ scripts/deployPolygon.js
@@ -1,14 +1,17 @@
 const { ethers } = require("hardhat");
 
 const ChildContractAddressTestnet = "0xb5505a6d998549090530911180f38aC5130101c6";
+const ChildContractAddressMainnet = "0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa";
+const ADMIN_WALLET_ADDRESS = process.env.ADMIN_WALLET_ADDRESS
 
 async function main() {
+    console.log("ADMIN WALLET:", ADMIN_WALLET_ADDRESS)
     const [deployer] = await ethers.getSigners();
     console.log("Deploying contracts with the account:", deployer.address);
     console.log("Account balance:", (await deployer.getBalance()).toString());
 
     const TypeVEnergyPolygonContract = await ethers.getContractFactory("TypeVEnergyPolygon");
-    const token = await TypeVEnergyPolygonContract.deploy([], ChildContractAddressTestnet)
+    const token = await TypeVEnergyPolygonContract.deploy([], ADMIN_WALLET_ADDRESS,ChildContractAddressMainnet)
 
     console.log("Token address:", token.address)
 }
